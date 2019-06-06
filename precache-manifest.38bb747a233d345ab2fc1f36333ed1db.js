d5e175d92140f95c968c88c55b42b6cb7bb6dc8d
Steffan Sluis <steffansluis@gmail.com>
2019-06-06 04:14:25 +0200

    Updates

---
+++ precache-manifest.38bb747a233d345ab2fc1f36333ed1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290f8d3814da9e2304a3161629e58a37",
+    "url": "/unsulliedcrowd-ui/index.html"
+  },
+  {
+    "revision": "316d3ff7030784606b0d",
+    "url": "/unsulliedcrowd-ui/static/css/main.11a9b018.chunk.css"
+  },
+  {
+    "revision": "e423b751c2c39a809804",
+    "url": "/unsulliedcrowd-ui/static/js/2.4b25fea1.chunk.js"
+  },
+  {
+    "revision": "316d3ff7030784606b0d",
+    "url": "/unsulliedcrowd-ui/static/js/main.8f24de5f.chunk.js"
+  },
+  {
+    "revision": "a3c8bebea6cd948f4326",
+    "url": "/unsulliedcrowd-ui/static/js/runtime~main.aaefc6c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unsulliedcrowd-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
